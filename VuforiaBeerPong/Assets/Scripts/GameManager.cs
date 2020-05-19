f4333013ff34d6e2dfd20976155babb66693a300
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-05-19 19:39:19 +0200

    jijijijji

--- VuforiaBeerPong/Assets/Scripts/GameManager.cs
+++ VuforiaBeerPong/Assets/Scripts/GameManager.cs
@@ -64,6 +64,11 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!focusModeSet)
+            Debug.Log("Failed to set focus mode");
+
+        Screen.SetResolution(1080, 1920, false);
         audioSource = GetComponent<AudioSource>();
         ballScript = ball.GetComponent<BallScript>();
     }
